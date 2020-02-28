33576d1591f657344c09df1bac0e74156f4381aa
chris <chrisreesjacobs@gmail.com>
2020-02-28 10:59:14 +0000

    validation of email and username, no testing; no checking for unique
    Signed-off-by: chris <chrisreesjacobs@gmail.com>

--- validation.js
+++ validation.js
@@ -1,13 +1,24 @@
 function userNameValidation(inputString) {
-
+    if (inputString.match(/[^a-zA-Z0-9]/g)) {
+        throw new Error("Username can only be alphanumeric")
+    }
 }
-
 //need to confirm unique in db
 //minimum length
 //maximum length?
 //alphanumeric only?
 
 function emailValidation(inputString) {
+    if (inputString.match(/\s/g)) {
+        throw new Error("Cannot have a space in the email")
+    }
+    if (inputString.match(/@[A-Za-z]+/g) == null) {
+        throw new Error("Does not have a domain")
+    }
+    if (inputString.match(/\.[A-Za-z.]+/g) == null) {
+        throw new Error("Email must have a network")
+    }
+
 
 }
 
