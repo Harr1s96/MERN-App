b2bfac672aca3077592a4420806bd14e2fd945db
chris <chrisreesjacobs@gmail.com>
2020-02-28 10:01:46 +0000

    plan for functions
    Signed-off-by: chris <chrisreesjacobs@gmail.com>

---
+++ validation.js
@@ -0,0 +1,20 @@
+function userNameValidation(string) {
+}
+
+//need to confirm unique in db
+//minimum length
+//maximum length?
+//alphanumeric only?
+
+function emailValidation(string) {
+}
+
+//need to confirm unique in db
+//must have String@Domain.Somewhere
+
+
+function passwordValidation(string) {
+//Minimum length
+//Alphanumeric + special
+//
+}
