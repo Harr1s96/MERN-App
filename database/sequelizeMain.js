1b116312414dd90bef41e0304c6c223db84a6d18
chris <chrisreesjacobs@gmail.com>
2020-02-28 14:34:29 +0000

    validation added into backend
    Signed-off-by: chris <chrisreesjacobs@gmail.com>

--- database/sequelizeMain.js
+++ database/sequelizeMain.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const userModel = require("./userModel");
-const mysql2 = require("mysql2");
+// const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize("userDB", "dbuser", "password", {
     host: "localhost",
